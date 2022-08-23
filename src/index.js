696f323291af5ad54aa1a22c5e83a2ea46571e25
nagaarvind <nagaaravind905@gmail.com>
2022-08-22 19:41:31 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <h1 className="Name-color">
+          Made with ❤️ by {""} 
+          <a
+              href="https://my-portfolio-arvinddev.vercel.app/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Naga Arvind
+            </a>
+          
+        </h1> 
+  </React.StrictMode>
+);
